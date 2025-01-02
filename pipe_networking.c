fdc915431ba58d5bd7094c30af2386e8d6b5655b
echen500 <echen50@stuy.edu>
2025-01-01 20:41:02 -0500

    renamed variables and fixed logic

--- pipe_networking.c
+++ pipe_networking.c
@@ -12,6 +12,7 @@
 #include "pipe_networking.h"
 #define READ 0
 #define WRITE 1
+
 //UPSTREAM = to the server / from the client
 //DOWNSTREAM = to the client / from the server
 /*=========================
@@ -23,12 +24,15 @@
   returns the file descriptor for the upstream pipe.
   =========================*/
 int server_setup() {
-  mkfifo(WKP, 0666);
-  open(WKP, O_RDONLY);
-  int from_client = 0;
-  read(WKP,&from_client, sizeof(from_client));
-  close(WKP);
-  unlink(WKP);
+  if (mkfifo(WKP, 0666) == -1) {
+    perror("error creating WKP");
+    exit(1);
+  }
+  int from_client = open(WKP, O_RDONLY);
+  if (from_client == -1) {
+    perror("error opening WKP");
+    exit(1);
+  }
   return from_client;
 }
 
@@ -43,20 +47,36 @@ int server_setup() {
   =========================*/
 int server_handshake(int *to_client) {
   int from_client = server_setup();
-  char pid[256];
-  sprintf(pid, "%d", from_client);
-  open(pid, O_WRONLY);
-  srand(NULL);
-  int num = rand() % 100000;
-  write(pid,num, sizeof(int));
-  open(WKP, O_RDONLY);
-  int ack;
-  read(WKP, &ack, sizeof(ack));
-  if (ack == num + 1) {
-    return from_client;
+
+  char client_pipe[256];
+  if (read(from_client, client_pipe, sizeof(client_pipe)) <= 0) {
+    perror("Error reading client pipe name");
+    unlink(WKP);
+    exit(1);
   }
-  perror("Handshake failed");
-  exit(1);
+  unlink(WKP);
+
+  *to_client = open(client_pipe, O_WRONLY);
+  if (*to_client == -1) {
+    perror("Error opening client pipe");
+    exit(1);
+  }
+
+  int random_num = rand() % 100000;
+  if (write(*to_client, &random_num, sizeof(random_num)) == -1) {
+    perror("Error writing to client");
+    exit(1);
+  }
+
+  int ack;
+  if (read(from_client, &ack, sizeof(ack)) <= 0 || ack != random_num + 1) {
+    perror("Handshake failed");
+    close(*to_client);
+    exit(1);
+  }
+
+  printf("Server: Handshake successful.\n");
+  return from_client;
 }
 
 
@@ -70,21 +90,53 @@ int server_handshake(int *to_client) {
   returns the file descriptor for the downstream pipe.
   =========================*/
 int client_handshake(int *to_server) {
-  int pid = getpid();
-  char PP[256];
-  sprintf(PP, "%d", pid);
-  mkfifo(PP, 0666); // make private pipe
-  open(WKP, O_WRONLY);
-  write(WKP, pid, sizeof(int));
-  close(WKP);
-  open(PP, O_RDONLY);
-  int from_server;
-  read(PP, &from_server, sizeof(int));
-  unlink(PP);
-  open(WKP, O_WRONLY);
-  int check = from_server + 1;
-  write(WKP, &check, sizeof(int));
-  return from_server;
+  char private_pipe[256];
+  sprintf(private_pipe, "%d", getpid());
+
+  if (mkfifo(private_pipe, 0666) == -1) {
+    perror("Error creating private pipe");
+    exit(1);
+  }
+
+  *to_server = open(WKP, O_WRONLY);
+  if (*to_server == -1) {
+    perror("Error connecting to server");
+    unlink(private_pipe);
+    exit(1);
+  }
+
+  if (write(*to_server, private_pipe, sizeof(private_pipe)) == -1) {
+    perror("Error sending PP name");
+    unlink(private_pipe);
+    exit(1);
+  }
+
+  int private_fd = open(private_pipe, O_RDONLY);
+  if (private_fd == -1) {
+    perror("Error opening PP");
+    unlink(private_pipe);
+    exit(1);
+  }
+
+  int server_num;
+  if (read(private_fd, &server_num, sizeof(server_num)) <= 0) {
+    perror("Error reading from server");
+    unlink(private_pipe);
+    close(private_fd);
+    exit(1);
+  }
+
+  int ack = server_num + 1;
+  if (write(*to_server, &ack, sizeof(ack)) == -1) {
+    perror("Error sending ack to server");
+    unlink(private_pipe);
+    close(private_fd);
+    exit(1);
+  }
+
+  unlink(private_pipe);
+  printf("Client: Handshake successful.\n");
+  return private_fd;
 }
 
 
@@ -97,7 +149,34 @@ int client_handshake(int *to_server) {
   returns the file descriptor for the downstream pipe.
   =========================*/
 int server_connect(int from_client) {
-  int to_client  = 0;
+  char client_pipe[256];
+  
+  if (read(from_client, client_pipe, sizeof(client_pipe)) <= 0) {
+    perror("Error reading client pipe name");
+    exit(1);
+  }
+
+  int to_client = open(client_pipe, O_WRONLY);
+  if (to_client == -1) {
+    perror("Error opening client pipe");
+    exit(1);
+  }
+
+  int random_num = rand() % 100000;
+  if (write(to_client, &random_num, sizeof(random_num)) == -1) {
+    perror("Error writing to client");
+    close(to_client);
+    exit(1);
+  }
+
+  int ack;
+  if (read(from_client, &ack, sizeof(ack)) <= 0 || ack != random_num + 1) {
+    perror("ERROR: Handshake failed");
+    close(to_client);
+    exit(1);
+  }
+
+  printf("Subserver: Handshake with client successful.\n");
   return to_client;
 }
 
