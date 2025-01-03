04aa95a91844bb6bbf9af6baadd95b82d300ffb8
echen500 <echen50@stuy.edu>
2025-01-03 13:53:05 -0500

    still not working

--- persistant_server.c
+++ persistant_server.c
@@ -5,13 +5,15 @@
 int main() {
   int to_client;
   int from_client;  
+  from_client = server_handshake( &to_client );
   while(1) {
-    while(from_client = server_handshake( &to_client )) {
+    while(to_client) {
         srand(time(NULL));
         int random_num = rand() % 100000000;
          write(to_client, &random_num, sizeof(random_num));
         sleep(1);
     }
+    from_client = server_handshake( &to_client );
   }
   
 }
