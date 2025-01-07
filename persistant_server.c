9e11f00bb6b63b093847e3aad751acb0b64ff2a4
echen500 <echen50@stuy.edu>
2025-01-06 19:41:44 -0500

    fixed persistant _server

--- persistant_server.c
+++ persistant_server.c
@@ -1,25 +1,33 @@
 #include "pipe_networking.h"
 #include <time.h>
 #include <fcntl.h>
+#include <unistd.h>
+#include <signal.h>
 
 int main() {
   int to_client;
-  int from_client;  
-  //from_client = server_handshake( &to_client );
-  while(1) {
-    from_client = server_handshake( &to_client );
-    while(1) {
-        srand(time(NULL));
-        int random_num = rand() % 100000000;
-        if (write(to_client, &random_num, sizeof(random_num)) == 0) {
-            close(to_client);
-            close(from_client);
-            unlink(to_client);
-            break;
-        }
-        sleep(1);
-    
+  int from_client;
+
+  signal(SIGPIPE, SIG_IGN);
+
+  srand(time(NULL));
+
+  while (1) {
+    from_client = server_handshake(&to_client);
+    while (1) {
+      int random_num = rand() % 100000000;
+      
+      if (write(to_client, &random_num, sizeof(random_num)) == -1) {
+        close(to_client);
+        close(from_client);
+       
+        break;
+      }
+
+      sleep(1);
     }
   }
-  
+
+  return 0;
 }
+ 
